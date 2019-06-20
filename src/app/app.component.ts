3742d88fff3389251f48dd5323d1493dfed06643
storres8 <stevetorres7562@gmail.com>
2019-06-20 13:13:55 -0400

    added button functionality and genereated new value component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'directives-assignment';
+  title = "directives-assignment";
+  notClicked = true;
+  display = "Display";
+
+  array = [];
+
+  handleClick() {
+    this.notClicked = !this.notClicked;
+    this.notClicked ? (this.display = "Display") : (this.display = "Hide");
+    this.array.push("Lorem");
+    console.log(this.array);
+  }
 }
